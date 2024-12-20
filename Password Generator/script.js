cd565165f397180260dd5aeafe1820dc1dc2fded
Thyeris <109691373+TH3ERIS45@users.noreply.github.com>
2024-12-20 17:05:18 -0300

    Add files via upload

---
+++ Password Generator/script.js
@@ -0,0 +1,36 @@
+function generatePassword() {
+    const length = document.getElementById('passwordLength').value;
+    const includeLowercase = document.getElementById('includeLowercase').checked;
+    const includeUppercase = document.getElementById('includeUppercase').checked;
+    const includeNumbers = document.getElementById('includeNumbers').checked;
+    const includeSymbols = document.getElementById('includeSymbols').checked;
+
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars = "0123456789";
+    const symbolChars = "!@#$%¨&*()-_=+><:;{}[]`^~";
+
+    let allowedChars = "";
+    let password = "";
+
+    allowedChars += includeLowercase ? lowercaseChars : "";
+    allowedChars += includeUppercase ? uppercaseChars : "";
+    allowedChars += includeNumbers ? numberChars : "";
+    allowedChars += includeSymbols ? symbolChars : "";
+
+    if (length < 1) {
+        alert("Password length must be at least 1");
+        return;
+    }
+    if (allowedChars.length === 0) {
+        alert("At least one set of characters must be selected");
+        return;
+    }
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * allowedChars.length);
+        password += allowedChars[randomIndex];
+    }
+
+    document.getElementById('passwordResult').value = password;
+}
